8025067b4b1de2666699e4677b4868c4d4d8f348
treshnikov <treshnikov@gmail.com>
2022-04-25 22:32:25 +0400

    Add fontawesome

--- src/Client/src/components/NavBar.tsx
+++ src/Client/src/components/NavBar.tsx
@@ -1,10 +1,12 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro'
 import React from 'react'
 
 export const NavBar: React.FunctionComponent = () => {
   return (
-    <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+    <nav className="navbar navbar-expand-md navbar-light bg-light">
     <div className="container-fluid">
-      <a className="navbar-brand" href="#">Taskly</a>
+      <a className="navbar-brand" href="#"><FontAwesomeIcon icon={solid('user-secret')} /> Taskly</a>
       <div>
         <ul className="navbar-nav me-auto mb-2 mb-md-0">
           <li className="nav-item">
