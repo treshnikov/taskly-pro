20f63eddc1e9f028c3398456ba3d1bee3152bbbc
treshnikov <treshnikov@gmail.com>
2022-05-03 23:23:13 +0400

    Display users in table

---
+++ src/Client/src/models/UserVm.ts
@@ -0,0 +1,5 @@
+export class UserVm {
+    id: string = '';
+    name: string = '';
+    email: string = '';
+}
--- src/Client/src/pages/Users.tsx
+++ src/Client/src/pages/Users.tsx
@@ -1,14 +1,74 @@
-import { Container } from '@mui/material';
-import React from 'react'
+import { Box, Container } from '@mui/material';
+import React, { useContext, useEffect, useState } from 'react'
+import { AuthContext } from '../context/AuthContext';
+import { UserVm } from "../models/UserVm";
+import { DataGrid, GridColDef, GridEventListener, GridEvents, GridRowModel, GridRowParams, GridValueGetterParams, MuiEvent } from '@mui/x-data-grid';
 
 export const Users: React.FunctionComponent = () => {
+  const { request } = useContext(AuthContext)
+  const [users, setUsers] = useState<UserVm[]>([])
 
+  useEffect(() => {
+    async function populateUsers() {
+      const json = await request("/api/v1/auth/users")
+      setUsers(json)
+    }
+    populateUsers()
+  }, [])
+
+  const columns: GridColDef[] = [
+    {
+      field: 'id',
+      headerName: 'ID',
+      minWidth: 150
+    },
+    {
+      field: 'name',
+      headerName: 'Name',
+      minWidth: 150,
+      editable: true,
+    },
+    {
+      field: 'email',
+      headerName: 'Email',
+      minWidth: 150,
+      editable: true,
+    }
+  ];
+
+  const handleRowEditStart = (params: GridRowParams, event: MuiEvent<React.SyntheticEvent>) => {
+    //event.defaultMuiPrevented = true;
+    //console.log("handleRowEditStart", params, event)
+  };
+
+  const handleRowEditStop: GridEventListener<GridEvents.rowEditStop> = (params, event) => {
+    //event.defaultMuiPrevented = true;
+    //console.log("handleRowEditStop", params, event)
+  };
+
+  const processRowUpdate = async (newRow: GridRowModel) => {
+    //console.log("processRowUpdate", newRow)
+    return { ...newRow, isNew: false };
+  };
 
   return (
     <div>
-      <Container maxWidth="lg">
-        <h1>Users</h1>
-      </Container>
+      <Box>
+        <div style={{ width: '100%' }}>
+          <DataGrid
+            autoHeight
+            rows={users}
+            columns={columns}
+            density='compact'
+            pageSize={100}
+            rowsPerPageOptions={[25, 50, 100]}
+            editMode='row'
+            onRowEditStart={handleRowEditStart}
+            onRowEditStop={handleRowEditStop}
+            processRowUpdate={processRowUpdate}
+          />
+        </div>
+      </Box>
     </div>
   )
 }
