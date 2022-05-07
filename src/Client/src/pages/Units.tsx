331293d3da32252dfa675f02292dd648d0f19819
treshnikov <treshnikov@gmail.com>
2022-05-07 22:51:53 +0400

    Add expand and collapse buttons for Units page

--- src/Client/src/pages/Units.tsx
+++ src/Client/src/pages/Units.tsx
@@ -1,6 +1,6 @@
 import TreeView from '@mui/lab/TreeView';
 import TreeItem from '@mui/lab/TreeItem';
-import { Container } from '@mui/material';
+import { Button, Container, Stack } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
@@ -20,18 +20,19 @@ const initData: RenderTree = {
 
 export default function Units() {
     const { request } = useContext(AuthContext)
-    const [data, setData] = useState<RenderTree>(initData)
+    const [units, setUnits] = useState<RenderTree>(initData)
+    const [expanded, setExpanded] = useState<string[]>([])
+    const { t } = useTranslation();
 
     useEffect(() => {
 
         async function fetchUnits() {
             const units = await request("/api/v1/units")
-            setData(units)
+            setUnits(units)
         }
 
         fetchUnits()
     }, [request])
-
 
     const renderTree = (nodes: RenderTree) => (
         <TreeItem key={nodes.id} nodeId={nodes.id} label={nodes.name}>
@@ -40,20 +41,46 @@ export default function Units() {
                 : null}
         </TreeItem>
     );
+    const handleToggle = (event: React.SyntheticEvent, nodeIds: string[]) => {
+        setExpanded(nodeIds);
+    };
 
-    const { t } = useTranslation();
+    const expandAll = () => {
+        let neweExpanded: string[] = []
+
+        function trace(current: RenderTree, neweExpanded: string[]) {
+            if (!current)
+                return
+
+            neweExpanded.push(current.id)
+            if (!current.children)
+                return
+
+            current.children.forEach(c => {
+                trace(c, neweExpanded)
+            });
+        }
+
+        trace(units, neweExpanded)
+        setExpanded(neweExpanded)
+    }
 
     return (
         <Container>
             <h3>{t('units')}</h3>
+            <Stack spacing={1} paddingBottom={1} direction="row">
+                <Button onClick={e => { expandAll() }} variant='contained'>{t('expand-all')}</Button>
+                <Button onClick={e => { setExpanded([]) }} variant='contained'>{t('collapse-all')}</Button>
+            </Stack>
             <TreeView
                 aria-label="rich object"
+                expanded={expanded}
+                onNodeToggle={handleToggle}
                 defaultCollapseIcon={<ChevronRightIcon />}
-                defaultExpanded={['root']}
                 defaultExpandIcon={<ExpandMoreIcon />}
                 sx={{ flexGrow: 1, overflowY: 'auto' }}
             >
-                {renderTree(data)}
+                {renderTree(units)}
             </TreeView>
         </Container>
     );
