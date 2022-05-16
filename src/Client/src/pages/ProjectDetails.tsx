2df3dbd4b44a7d14979cbddbae4c39c3ac00305c
treshnikov <treshnikov@gmail.com>
2022-05-16 17:51:03 +0400

    Refactoring ProjectDetails page

--- src/Client/src/pages/ProjectDetails.tsx
+++ src/Client/src/pages/ProjectDetails.tsx
@@ -1,16 +1,16 @@
-import React, { useLayoutEffect, useState } from 'react'
+import React, { useEffect, useLayoutEffect, useState } from 'react'
 import HotTable, { HotColumn } from '@handsontable/react';
 import { registerAllModules } from 'handsontable/registry';
 import { useTranslation } from 'react-i18next';
 import { useHttp } from '../hooks/http.hook';
 import { useParams } from 'react-router-dom';
 import { ProjectDetailedInfoVm } from "../models/ProjectDetailedInfoVm";
-import { ProjectTaskVm } from "../models/ProjectTaskVm";
-import { ProjectTaskUnitEstimationVm } from "../models/ProjectTaskUnitEstimationVm";
 import { DepartmentsCellRenderer } from "../components/ProjectDetails/DepartmentsCellRenderer"
 import { WeekCellRenderer } from '../components/ProjectDetails/WeekCellRenderer';
 import { Stack } from '@mui/material';
 import { DateCellRenderer } from '../components/ProjectDetails/DateCellRenderer';
+import { ProjectTaskVm } from '../models/ProjectTaskVm';
+import { ProjectTaskUnitEstimationVm } from '../models/ProjectTaskUnitEstimationVm';
 
 registerAllModules();
 
@@ -20,67 +20,34 @@ export const ProjectDetails: React.FunctionComponent = () => {
   const { t } = useTranslation();
 
   const staticHeaders = ['', t('task'), t('start'), t('end'), t('units')]
-  
-  const [tasks, setTasks] = useState<ProjectTaskVm[]>([])
+
+  const [projectInfo, setProjectInfo] = useState<ProjectDetailedInfoVm>(new ProjectDetailedInfoVm())
   const [headers, setHeaders] = useState<string[]>(staticHeaders)
   const [colWidths, setColWidths] = useState<number[]>([25, 350, 100, 100, 310])
   const [firstMonday, setFirstMonday] = useState<Date>(new Date())
+  const [tableHeight, setTableHeight] = useState<number>(500)
 
-  useLayoutEffect(() => {
-    let projectInfo = new ProjectDetailedInfoVm()
+  useEffect(() => {
     async function requestDetails() {
-      projectInfo = await request<ProjectDetailedInfoVm>("/api/v1/projects/" + projectId)
-
-      let newTasks = projectInfo.tasks
-
-      const testTask = new ProjectTaskVm()
-      testTask.description = "Монтажные и пусконаладочные работы схемы управления разъединителями ОРУ-110 кВ"
-      testTask.start = new Date(2022, 3, 5, 0, 0, 0, 0)
-      testTask.end = new Date(2022, 4, 8, 0, 0, 0, 0)
-
-      const testEstimation1 = new ProjectTaskUnitEstimationVm()
-      testEstimation1.id = "asdfasdfsdf"
-      testEstimation1.unitName = "Отдел программирования РСУ"
-      testEstimation1.chiefSpecialistHours = 120
-      testEstimation1.leadEngineerHours = 40
-      testEstimation1.engineerOfTheSecondCategoryHours = 90
-      testEstimation1.engineerOfTheFirstCategoryHours = 40
-      testEstimation1.engineerOfTheThirdCategoryHours = 120
-      testEstimation1.techniclaWriterHours = 900
-
-      const testEstimation2 = new ProjectTaskUnitEstimationVm()
-      testEstimation2.id = "dfgsdfg"
-      testEstimation2.unitName = "Отдел программирования СУПП"
-      testEstimation2.chiefSpecialistHours = 800
-
-      const testEstimation3 = new ProjectTaskUnitEstimationVm()
-      testEstimation3.id = "iuthoi3hti2hpi"
-      testEstimation3.unitName = "Отдел программирования с очень длинным именем"
-      testEstimation3.engineerOfTheThirdCategoryHours = 16
-
-      testTask.estimations = [testEstimation1, testEstimation2, testEstimation3]
-
-      newTasks = [...newTasks, testTask]
-      projectInfo.tasks = newTasks
-      ProjectDetailedInfoVm.init(projectInfo)
-      setFirstMonday(projectInfo.weeks[0]?.monday)
-
-      setTasks(newTasks)
+      let data = await request<ProjectDetailedInfoVm>("/api/v1/projects/" + projectId)
+      populateDemoTasks(data)
+      ProjectDetailedInfoVm.init(data)
+      setProjectInfo(data)
     }
-
-    requestDetails().then(arg => {
-      const weekHeaders = projectInfo.weeks.map((i, idx) => {
-        return i.monday.toLocaleDateString()
-      })
-      const weekColsWidths = projectInfo.weeks.map(i => 80)
-
-      setHeaders(h => [...h, ...weekHeaders])
-      setColWidths(c => [...c, ...weekColsWidths])
-    })
+    requestDetails()
   }, [request, projectId])
 
+  useEffect(() => {
+    const weekHeaders = projectInfo.weeks.map((i, idx) => {
+      return i.monday.toLocaleDateString()
+    })
+    const weekColsWidths = projectInfo.weeks.map(i => 80)
 
-  const [tableHeight, setTableHeight] = useState<number>(500)
+    setFirstMonday(projectInfo.weeks[0]?.monday)
+    setHeaders(h => [...h, ...weekHeaders])
+    setColWidths(c => [...c, ...weekColsWidths])
+  }, [projectInfo])
+
   useLayoutEffect(() => {
     setTableHeight(window.innerHeight - 145)
   }, [])
@@ -97,7 +64,7 @@ export const ProjectDetails: React.FunctionComponent = () => {
           renderAllRows={true}
           viewportColumnRenderingOffset={headers.length}
           fixedColumnsLeft={staticHeaders.length}
-          data={tasks}
+          data={projectInfo.tasks}
           colWidths={colWidths}
           colHeaders={headers}
           wordWrap={true}
@@ -109,8 +76,10 @@ export const ProjectDetails: React.FunctionComponent = () => {
         >
           <HotColumn hiddenColumns data={"id"} editor={false} type={"text"} />
           <HotColumn data={"description"} type={"text"} />
-          {/* <HotColumn data={"start"} type={"date"} dateFormat='DD.MM.YYYY' correctFormat={true} defaultDate='01.01.2022' />
-          <HotColumn data={"end"} type={"date"} dateFormat='DD.MM.YYYY' correctFormat={true} defaultDate='01.01.2022' /> */}
+          {/* 
+            <HotColumn data={"start"} type={"date"} dateFormat='DD.MM.YYYY' correctFormat={true} defaultDate='01.01.2022' />
+            <HotColumn data={"end"} type={"date"} dateFormat='DD.MM.YYYY' correctFormat={true} defaultDate='01.01.2022' /> 
+          */}
           <HotColumn data={"start"} readOnly >
             <DateCellRenderer hot-renderer></DateCellRenderer>
           </HotColumn>
@@ -134,3 +103,38 @@ export const ProjectDetails: React.FunctionComponent = () => {
     </div>
   )
 }
+
+function populateDemoTasks(projectInfo: ProjectDetailedInfoVm) {
+  let newTasks = projectInfo.tasks
+
+  const testTask = new ProjectTaskVm()
+  testTask.description = "Монтажные и пусконаладочные работы схемы управления разъединителями ОРУ-110 кВ"
+  testTask.start = new Date(2022, 3, 5, 0, 0, 0, 0)
+  testTask.end = new Date(2022, 4, 8, 0, 0, 0, 0)
+
+  const testEstimation1 = new ProjectTaskUnitEstimationVm()
+  testEstimation1.id = "asdfasdfsdf"
+  testEstimation1.unitName = "Отдел программирования РСУ"
+  testEstimation1.chiefSpecialistHours = 120
+  testEstimation1.leadEngineerHours = 40
+  testEstimation1.engineerOfTheSecondCategoryHours = 90
+  testEstimation1.engineerOfTheFirstCategoryHours = 40
+  testEstimation1.engineerOfTheThirdCategoryHours = 120
+  testEstimation1.techniclaWriterHours = 900
+
+  const testEstimation2 = new ProjectTaskUnitEstimationVm()
+  testEstimation2.id = "dfgsdfg"
+  testEstimation2.unitName = "Отдел программирования СУПП"
+  testEstimation2.chiefSpecialistHours = 800
+
+  const testEstimation3 = new ProjectTaskUnitEstimationVm()
+  testEstimation3.id = "iuthoi3hti2hpi"
+  testEstimation3.unitName = "Отдел программирования с очень длинным именем"
+  testEstimation3.engineerOfTheThirdCategoryHours = 16
+
+  testTask.estimations = [testEstimation1, testEstimation2, testEstimation3]
+
+  newTasks = [...newTasks, testTask]
+  projectInfo.tasks = newTasks
+}
+
