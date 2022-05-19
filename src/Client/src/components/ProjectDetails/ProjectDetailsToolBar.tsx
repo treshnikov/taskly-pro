e4a0b8844f8870dce24f90e203ff69e030e08f2a
treshnikov <treshnikov@gmail.com>
2022-05-19 15:28:34 +0400

    Add translations for project details toolbar

--- src/Client/src/components/ProjectDetails/ProjectDetailsToolBar.tsx
+++ src/Client/src/components/ProjectDetails/ProjectDetailsToolBar.tsx
@@ -1,28 +1,33 @@
-import { Button, Checkbox, FormControlLabel, Grid, Stack, Typography } from "@mui/material"
+import { Button, Checkbox, Divider, FormControlLabel, Grid, Stack, Typography } from "@mui/material"
 import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd';
 import BarChartIcon from '@mui/icons-material/BarChart';
 import ZoomInIcon from '@mui/icons-material/ZoomIn';
 import ZoomOutIcon from '@mui/icons-material/ZoomOut';
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import { ganttZoomIn, ganttZoomOut, toggleShowDetails } from '../../redux/projectDetailsSlice';
+import { useTranslation } from "react-i18next";
 
 export const ProjectDetailsToolBar: React.FunctionComponent = () => {
+    const { t } = useTranslation();
     const dispatch = useAppDispatch()
     const showDetails = useAppSelector(state => state.projectDetailsReducer.showDetails)
     const projectShortName = useAppSelector(state => state.projectDetailsReducer.projectShortName)
+
     return (
         <Grid container  >
-            <Grid item xs={8} >
+            <Grid item xs={6} >
                 <Stack direction="row" spacing={1} paddingTop={1} paddingBottom={1}>
-                    <Button variant='contained' size='small' startIcon={<PlaylistAddIcon />}>Add</Button>
-                    <Button variant='contained' size='small' startIcon={<BarChartIcon />}>Statistics</Button>
-                    <Button variant='contained' size='small' startIcon={<ZoomInIcon />} onClick={e => { dispatch(ganttZoomIn()) }} >Zoom in</Button>
-                    <Button variant='contained' size='small' startIcon={<ZoomOutIcon />} onClick={e => { dispatch(ganttZoomOut()) }} >Zoom out</Button>
-                    <FormControlLabel label="Show details" control={<Checkbox checked={showDetails} onChange={e => { dispatch(toggleShowDetails()) }} size='small' />} />
+                    <Button variant='contained' size='small' startIcon={<PlaylistAddIcon />}>{t('add')}</Button>
+                    <Button variant='contained' size='small' startIcon={<BarChartIcon />}>{t('statistics')}</Button>
+                    <FormControlLabel label={t('show-details')} control={<Checkbox checked={showDetails} onChange={e => { dispatch(toggleShowDetails()) }} size='small' />} />
                 </Stack>
             </Grid>
-            <Grid item xs={4} style={{ textAlign: "right" }} paddingTop={1} paddingBottom={1}>
-                <Typography variant='h5'>{projectShortName}</Typography>
+            <Grid item xs={6} style={{ textAlign: "right" }} paddingTop={1} paddingBottom={1}>
+                <Stack direction="row" paddingTop={1} paddingBottom={1} justifyContent="flex-end" >
+                    <Button variant='text' size='small' startIcon={<ZoomInIcon />} onClick={e => { dispatch(ganttZoomIn()) }} ></Button>
+                    <Button variant='text' size='small' startIcon={<ZoomOutIcon />} onClick={e => { dispatch(ganttZoomOut()) }} ></Button>
+                    <Typography variant='h5'>{projectShortName}</Typography>
+                </Stack>
             </Grid>
         </Grid>
     )
