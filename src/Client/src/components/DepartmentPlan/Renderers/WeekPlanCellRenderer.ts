5a298abc50003f82644b79a98d7f4d8ad1a4ac3d
treshnikov <treshnikov@gmail.com>
2022-06-24 14:51:36 +0400

    Update styles

--- src/Client/src/components/DepartmentPlan/Renderers/WeekPlanCellRenderer.ts
+++ src/Client/src/components/DepartmentPlan/Renderers/WeekPlanCellRenderer.ts
@@ -18,23 +18,22 @@ export const WeekPlanCellRenderer = (instance: Handsontable.Core, td: HTMLTableC
             planTime = planTime * rateAsNumber
         }
 
-        const valueAsFloat = parseFloat(value)
-        if (!isNaN(valueAsFloat)) {
-            td.style.fontWeight = '500'
-            if (valueAsFloat === planTime) {
-                td.style.background = '-webkit-linear-gradient(bottom, #ecffebaa 100%, white 100%)'
-            }
-            else {
-                if (valueAsFloat > planTime) {
-                    td.style.background = '-webkit-linear-gradient(bottom, #ffcccc88 100%, white 100%)'
-                }
-                else {
-                    td.style.background = '-webkit-linear-gradient(bottom, #ffffe0aa 100%, white 100%)'
-                }
-            }
+        let valueAsFloat = parseFloat(value)
+        if (isNaN(valueAsFloat)) {
+            valueAsFloat = 0
+        }
+
+        td.style.fontWeight = '500'
+        if (valueAsFloat === planTime) {
+            td.style.background = '-webkit-linear-gradient(bottom, #ecffebaa 100%, white 100%)'
         }
         else {
-            td.style.background = '-webkit-linear-gradient(bottom, #f8f8f8 100%, white 100%)'
+            if (valueAsFloat > planTime) {
+                td.style.background = '-webkit-linear-gradient(bottom, #ffcccc88 100%, white 100%)'
+            }
+            else {
+                td.style.background = '-webkit-linear-gradient(bottom, #ffffe0aa 100%, white 100%)'
+            }
         }
     }
 }
