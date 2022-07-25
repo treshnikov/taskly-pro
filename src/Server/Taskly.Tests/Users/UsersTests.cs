55ad8cc4ddd5f61869321dcaf0f47b2ff2bd21ce
treshnikov <treshnikov@gmail.com>
2022-07-25 15:52:04 +0400

    Add unit tests

--- src/Server/Taskly.Tests/Users/UsersTests.cs
+++ src/Server/Taskly.Tests/Users/UsersTests.cs
@@ -20,5 +20,21 @@ public class UsersTests : TestCommandBase
 
         // assert
         Assert.NotNull(users.FirstOrDefault(u => u.Name == "admin"));
+    }
+
+    [Fact]
+    public async Task UserCanBeFoundById()
+    {
+        // arrange
+        var users = await new GetUsersRequestHandler(Context, Mapper)
+            .Handle(new GetUsersRequest(), CancellationToken.None);
+        var admin = users.First(u => u.Name == "admin");
+
+        //act
+        var user = await new GetUserRequestHandler(Context, Mapper)
+            .Handle(new GetUserRequest { UserId = admin.Id }, CancellationToken.None);
+
+        // assert
+        Assert.NotNull(user);
     }    
 }
