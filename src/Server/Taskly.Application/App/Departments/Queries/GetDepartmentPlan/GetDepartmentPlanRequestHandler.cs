fe22c9935513cc598b70251b25c5df9055311721
treshnikov <treshnikov@gmail.com>
2022-10-06 00:22:21 +0400

    Handle user quit date

--- src/Server/Taskly.Application/App/Departments/Queries/GetDepartmentPlan/GetDepartmentPlanRequestHandler.cs
+++ src/Server/Taskly.Application/App/Departments/Queries/GetDepartmentPlan/GetDepartmentPlanRequestHandler.cs
@@ -177,6 +177,11 @@ namespace Taskly.Application.Departments.Queries.GetDepartmentPlan
 
                 day.HoursAvailableForPlanning *= user.Rate;
 
+                if (user.User.QuitDate is not null && dt >= user.User.QuitDate)
+                {
+                    day.HoursAvailableForPlanning = 0;
+                }
+
                 res.Add(day);
                 dt = dt.AddDays(7);
             }
