826ff1b6bbaca2c79f2b3a03a9b6022ee9d3944c
treshnikov <treshnikov@gmail.com>
2022-10-12 11:48:40 +0400

    Add a template for week summary report

--- src/Server/Taskly.Application/App/Reports/Queries/GetWeekSummaryReport/GetWeekSummaryReportRequestHandler.cs
+++ src/Server/Taskly.Application/App/Reports/Queries/GetWeekSummaryReport/GetWeekSummaryReportRequestHandler.cs
@@ -30,9 +30,19 @@ public class GetWeekSummaryReportRequestHandler : IRequestHandler<GetWeekSummary
             var depUsers = await _dbContext.Users
                 .Include(i => i.UserDepartments)
                 .Where(u =>
+                    // todo extract logic to a specification
                     u.HiringDate <= request.Monday &&
                     (u.QuitDate == null || u.QuitDate > request.Monday.AddDays(5)) &&
                     u.UserDepartments.Any(ud => ud.DepartmentId == dep.Id && ud.Rate > 0.1))
+                .AsNoTracking()
+                .ToListAsync(cancellationToken);
+            var userIds = depUsers.Select(i => i.Id).ToList();
+
+            var userPlans = await _dbContext.DepartmentPlans
+                .Include(i => i.ProjectTask)
+                    .ThenInclude(i => i.Project)
+                .Where(p => p.WeekStart == request.Monday && userIds.Contains(p.UserId))
+                .OrderBy(p => p.ProjectTask.Project.Id).ThenByDescending(p => p.Hours)
                 .AsNoTracking()
                 .ToListAsync(cancellationToken);
 
@@ -44,10 +54,24 @@ public class GetWeekSummaryReportRequestHandler : IRequestHandler<GetWeekSummary
 
             foreach (var user in depUsers)
             {
-                var userVm = new WeekSummaryUserVm{
+                var userVm = new WeekSummaryUserVm
+                {
                     Name = user.Name,
-                    Rate = user.UserDepartments.Select(ud => ud.Rate).Max()
+                    Rate = user.UserDepartments.Select(ud => ud.Rate).Max(),
+                    Plans = new List<WeekSummaryPlanVm>()
                 };
+
+                foreach (var p in userPlans.Where(u => u.UserId == user.Id))
+                {
+                    userVm.Plans.Add(new WeekSummaryPlanVm
+                    {
+                        TaskName = $"{p.ProjectTask.Project.Id}: {p.ProjectTask.Project.ShortName} - {p.ProjectTask.Description}",
+                        Hours = p.Hours,
+                        TaskStart = p.ProjectTask.Start,
+                        TaskEnd = p.ProjectTask.End,
+                        TaskIsOutdated = !(request.Monday >= p.ProjectTask.Start && request.Monday <= p.ProjectTask.End)
+                    });
+                }
 
                 depVm.Users.Add(userVm);
             }
--- src/Server/Taskly.Application/App/Reports/Queries/GetWeekSummaryReport/WeekSummaryReportVm.cs
+++ src/Server/Taskly.Application/App/Reports/Queries/GetWeekSummaryReport/WeekSummaryReportVm.cs
@@ -15,6 +15,15 @@ namespace Taskly.Application.App.Reports
     {
         public string Name { get; set; }
         public double Rate { get; set; }
-        
+        public List<WeekSummaryPlanVm> Plans { get; set; }
+    }
+
+    public class WeekSummaryPlanVm
+    {
+        public string TaskName { get; set; }
+        public double Hours { get; set; }
+        public DateTime TaskStart { get; set; }
+        public DateTime TaskEnd { get; set; }
+        public bool TaskIsOutdated { get; set; }
     }
 }
